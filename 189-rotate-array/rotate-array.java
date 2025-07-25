f244175a85eae2253e13311bbbc77e07f45bba6e
PotharajuKalyani-218 <23951a0473@iare.ac.in>
2025-07-25 12:11:38 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (36.36%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n=nums.length;
+        /*int n=nums.length;
          k=k%n;
         int res[]=new int[n];
         for(int i=0;i<n;i++)
@@ -9,9 +9,21 @@ class Solution {
         }
         for(int i=0;i<n;i++){
             nums[i]=res[i];
+        } */
+        int n=nums.length;
+        k=k%n;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+    }
+    public void reverse(int [] A,int start,int end){
+        while(start<end)
+        {
+            int t=A[start];
+            A[start]=A[end];
+            A[end]=t;
+            start++;
+            end--;
         }
-
-
-        
     }
 }
