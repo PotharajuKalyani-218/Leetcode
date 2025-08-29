0541bd97a216e95f13be781300842e40365c6a09
PotharajuKalyani-218 <23951a0473@iare.ac.in>
2025-08-29 10:43:32 +0530

    Time: 1 ms (90.66%) | Memory: 42.2 MB (62.97%) - LeetSync

---
+++ 171-excel-sheet-column-number/excel-sheet-column-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int n=0;
+        for(int i=0;i<columnTitle.length();i++)
+        {
+            n=n*26+(columnTitle.charAt(i)-'A'+1);
+        }
+        return n;
+    
+        
+    }
+}
