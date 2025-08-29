10f1d03f3bddabae7bc3ddbb7ec88907c76407ea
PotharajuKalyani-218 <23951a0473@iare.ac.in>
2025-08-29 10:27:43 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (37.54%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder res=new StringBuilder();
+        while(columnNumber!=0)
+        {
+            columnNumber--;
+            int r=columnNumber%26;
+            columnNumber=columnNumber/26;
+            res.append((char)('A'+r));
+
+        }
+        return res.reverse().toString();
+        
+    }
+}
