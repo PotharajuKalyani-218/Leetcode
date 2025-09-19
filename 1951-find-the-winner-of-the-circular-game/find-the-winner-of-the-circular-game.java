f5a9dd32d68c1c07da3102b02d9e15c184d03119
PotharajuKalyani-218 <23951a0473@iare.ac.in>
2025-09-19 10:29:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (89.70%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,20 +1,10 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        Queue<Integer> q=new LinkedList<>();
+        int winner=0;
         for(int i=1;i<=n;i++)
         {
-            q.offer(i);
+            winner=(winner+k)%i;
         }
-        while(q.size()>1)
-        {
-            for(int i=0;i<k-1;i++)
-            {
-                q.offer(q.poll());
-            }
-            q.poll();
-        }
-
-       return  q.poll(); 
-
+        return winner+1;
     }
 }
