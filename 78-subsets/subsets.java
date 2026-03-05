34d89d90adcebac2672fadaa7a666b33d3dec609
PotharajuKalyani-218 <23951a0473@iare.ac.in>
2026-03-05 10:09:39 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (19.16%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,18 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        int n=nums.length;
-        List<List<Integer>> res=new ArrayList<>();
-        for(int i=0;i<(1<<n);i++)
-        {
-                List<Integer> sublist=new ArrayList<>();
-            for(int j=0;j<n;j++){
-                if((i&(1<<j))>0)
-                {
-                    sublist.add(nums[j]);
-                }
-            }
-            res.add(sublist);
-        }
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, res, new ArrayList<>(), 0);
         return res;
+    }
+
+    // The start parameter correctly guides the recursion to the next element
+    public void backtrack(int[] nums, List<List<Integer>> res, List<Integer> curr, int start) {
+        // 1. Add the current subset to the result list
+        res.add(new ArrayList<>(curr));
+
+        // 2. Iterate through remaining elements from the 'start' index
+        for (int i = start; i < nums.length; i++) {
+            // Include the current element
+            curr.add(nums[i]);
+            
+            // Recursively call backtrack with the next index (i + 1)
+            backtrack(nums, res, curr, i + 1); 
+            
+            // Backtrack: remove the last added element to explore other subsets
+            curr.remove(curr.size() - 1);
+        }
     }
 }
